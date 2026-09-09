e16178a0ba7056483dd11e07246b6e2803a8243c
MITHU SHRI K M <mithunshri2005@gmail.com>
2026-09-09 22:15:05 +0530

    Update: duplicate-integer - submission-0

--- Data Structures & Algorithms/duplicate-integer/submission-0.java
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -1,19 +1,22 @@
-class Solution {
-    public boolean hasDuplicate(int[] nums) {
-
+class Solution
+{
+    public boolean hasDuplicate(int []nums)
+    {
         Set<Integer>set=new HashSet<>();
-         
-         
-
-        for(int i=0;i<nums.length;i++)
-        {
-            if(set.contains(nums[i]))
-            {
-                return true;
-            }
-            set.add(nums[i]);
-        }
-        return false;
         
+            for(int num:nums)
+            {
+                
+
+                if(set.contains(num))
+                {
+                    return true;
+                
+               
+                }
+                 set.add(num);
+            }
+            return false;
+         
     }
 }
