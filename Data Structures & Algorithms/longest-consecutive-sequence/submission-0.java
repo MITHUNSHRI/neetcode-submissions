8008c709bda4025b8cd6a24365fcf516712270d8
MITHU SHRI K M <mithunshri2005@gmail.com>
2026-09-09 22:10:42 +0530

    Bulk sync: 8 submissions

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-0.java
@@ -0,0 +1,30 @@
+class Solution
+{
+    public int longestConsecutive(int[]nums)
+    {
+        Set<Integer>set=new HashSet<>();
+
+    for(int n:nums)
+    {
+        set.add(n);
+    }
+    
+    int longest=0;
+    
+  for(int n:set)
+  {
+    if(!set.contains(n-1))
+    {
+        int count=1;
+    
+    while(set.contains(n+count))
+    {
+        count++;
+    }
+
+    longest=Math.max(longest,count);
+    }
+}
+return longest;
+    }
+}
